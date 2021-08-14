822ee49bebfa83d2b0d41fc06f6fa25c85b35bfd
EC2 Default User <ec2-user@ip-172-31-54-208.ec2.internal>
2021-08-14 16:45:13 +0000

    Rails-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,7 +6,7 @@ class TodolistsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-     redirect_to todolist_path(list.id)
+     redirect_to todolist_path(@list.id)
     else
      render :new
     end
@@ -28,6 +28,12 @@ class TodolistsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
   end
 
 
